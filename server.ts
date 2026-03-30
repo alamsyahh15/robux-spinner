91f2bf56398bc4455b360f50d2431d08afa0ea6f
alamsyahh15 <black.taurus001@gmail.com>
2026-03-30 11:02:41 +0700

    update voucher code

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ if (!fs.existsSync(CHECKPOINT_FILE)) {
   fs.writeFileSync(CHECKPOINT_FILE, JSON.stringify({ robloxUsernames: [], discordUsernames: [] }, null, 2));
 }
 
-const VALID_VOUCHERS = ['CHOCONG69','COKLAT69'];
+const VALID_VOUCHERS = ['SPIN-MEEE','COKLAT69','CHOCONG69'];
 
 const REWARDS = [
   { amount: 6, weight: 1000000, color: '#f87171' }, // red-400
