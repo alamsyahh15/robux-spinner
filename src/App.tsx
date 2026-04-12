8cb55a6f52a9bf0e593512d8eb9c758a6bfc4c1b
alamsyahh15 <black.taurus001@gmail.com>
2026-04-12 15:13:38 +0700

    fix path

--- src/App.tsx
+++ src/App.tsx
@@ -282,12 +282,12 @@ export default function App() {
             <br /> <br/>
             Untuk claim Free Robux wajib join Discord MedusaBlox dan open ticket. Link Discord:{' '}
             <a
-              href={pathname === '/VDS99' ? 'https://discord.gg/5dmP2ASNQp' : 'https://discord.com/invite/Afs5b76ejR'}
+              href={pathname === '/VDS999' ? 'https://discord.gg/5dmP2ASNQp' : 'https://discord.com/invite/Afs5b76ejR'}
               target="_blank"
               rel="noreferrer"
               className="text-purple-300 hover:text-purple-200 underline"
             >
-              {pathname === '/VDS99' ? 'https://discord.gg/5dmP2ASNQp' : 'https://discord.com/invite/Afs5b76ejR'}
+              {pathname === '/VDS999' ? 'https://discord.gg/5dmP2ASNQp' : 'https://discord.com/invite/Afs5b76ejR'}
             </a>
           </p>
 
